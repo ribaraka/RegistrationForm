24c58707b3b1bf0c16d11bd4894ec7ec3ecaaef2
Stanislav Bychkov <stanislav.bychkov67@gmail.com>
2021-02-11 20:58:36 +0200

    start project

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    publicPath: '/',
+    port: 9000,
+    contentBase: path.join(__dirname, 'dist'),
+    host: '127.0.0.1',
+    hot: true,
+  },
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[hash].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
