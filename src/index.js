08f736b69fb54d4355f44260b912873cc501d944
Stanislav Bychkov <stanislav.bychkov67@gmail.com>
2021-02-12 17:10:14 +0200

    created all required variables. created htlm result file. handle function started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const buttonSign = document.querySelector('.sign');
+const form = document.querySelector('form');
+let isError = true;
+
+function handleSignUp(e) {
+if (isError){
+  e.preventDefault();
+}
+}
+
+form.addEventListener('submit', handleSignUp);
