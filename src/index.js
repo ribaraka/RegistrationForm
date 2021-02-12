10221c416b2143ba300554374280dbf84c75e0ed
Stanislav Bychkov <stanislav.bychkov67@gmail.com>
2021-02-12 19:29:46 +0200

    completed basic css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-const firstName = document.querySelector('#first-name');
-const password = document.querySelector('#password');
-const form = document.querySelector('form');
+import './assets/css/form.css';
 
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const buttonSign = document.querySelector('.sign');
+const form = document.querySelector('form');
+let isError = true;
+
+function handleSignUp(e) {
+  if (isError){
+    e.preventDefault();
+  }
+}
+
+form.addEventListener('submit', handleSignUp);
