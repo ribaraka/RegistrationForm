7b86ba998ecdc89c080145ef063692ccc09d9d6f
Stanislav Bychkov <stanislav.bychkov67@gmail.com>
2021-02-12 22:00:18 +0200

    created error for empty input

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
+import '/assets/css/form.css'
+
 const firstName = document.querySelector('#first-name');
 const lastName = document.querySelector('#last-name');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const buttonSign = document.querySelector('.sign');
+const buttonSign = document.querySelector('.button-sign');
 const form = document.querySelector('form');
 let isError = true;
+// const inputError = document.createElement('div');
+// inputError.className = 'error-message';
 
-function handleSignUp(e) {
-if (isError){
+function createError(div) {
+  const inputError = document.createElement('div');
+  inputError.className = 'error-message';
+  inputError.textContent = 'required';
+  div.parentNode.insertBefore(inputError, div);
+}
+
+function checkFirstName() {
+  const firstNameValue = firstName.value.trim();
+  if (!firstNameValue){
+    createError(firstName);
+   }
+}
+
+function checkLastName() {
+  const lastNameValue = lastName.value.trim();
+  if (!lastNameValue){
+    createError(lastName);
+  }
+}
+
+function checkEmailValue() {
+  const emailValue = email.value.trim();
+  isError = emailValue === '';
+}
+
+function checkPassword() {
+  const passwordValue = password.value.trim();
+  isError = passwordValue === '';
+  if (isError) {
+
+  }
+}
+
+function handleSignButton(e) {
+  checkFirstName();
+  checkLastName();
+  // checkEmailValue();
+  // checkPassword();
   e.preventDefault();
-}
+
 }
 
-form.addEventListener('submit', handleSignUp);
+buttonSign.addEventListener('click', handleSignButton);
+
